bb754992dde6275235cbd438fc9e58821686e84d
Lorenzo Dalvi <lorenzodalvi2410@gmail.com>
2022-09-13 09:41:33 -0300

    criando autenticação de usuários

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import GlobalStyle from './Styles/global';
 import RoutesApp from './Routes';
+import { AuthProvider } from './contexts/auth';
 
 
 const App = () => {
     return (
-        <>
+        <AuthProvider>
         <RoutesApp />
         <GlobalStyle /> 
-        </>
+        </AuthProvider>
     );
 };
 
